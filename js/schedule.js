19786df6ce49c1e0e73129d7c306ae96e58706a4
RomanYuen <jasonyuen1001@gmail.com>
2020-06-16 20:29:45 +0800

    schedule updated

--- js/schedule.js
+++ js/schedule.js
@@ -1,110 +1,103 @@
 var p0Schedule = [
-    '<p class="my-0 fw-700">000</p>' +
-    '<p class="my-0"><b>Team NW-A1</b></p>' +
-    '<p class="my-0 fs-12"><b>Magic Chain</b></p>',
+    '<p class="my-0 fw-700">14:30</p>' +
+    '<p class="my-0"><b>Team 102</b></p>',
 
 
-    '<p class="my-0 fw-700">000</p>' +
-    '<p class="my-0"><b>Team NW-A1</b></p>' +
-    '<p class="my-0 fs-12"><b>Magic Chain</b></p>',
+    '<p class="my-0 fw-700">14:30</p>' +
+    '<p class="my-0"><b>Team 380</b></p>',
 
 
-    '<p class="my-0 fw-700">000</p>' +
-    '<p class="my-0"><b>Team NW-A1</b></p>' +
-    '<p class="my-0 fs-12"><b>Magic Chain</b></p>',
+    '<p class="my-0 fw-700">14:30</p>' +
+    '<p class="my-0"><b>Team 917</b></p>',
 
-    '<p class="my-0 fw-700">000</p>' +
-    '<p class="my-0"><b>Team NW-A1</b></p>' +
-    '<p class="my-0 fs-12"><b>Magic Chain</b></p>',
+    '<p class="my-0 fw-700">14:30</p>' +
+    '<p class="my-0"><b>Team 999</b></p>',
 
 
-    '<p class="my-0 fw-700">000</p>' +
-    '<p class="my-0"><b>Team NW-A1</b></p>' +
-    '<p class="my-0 fs-12"><b>Magic Chain</b></p>'
+    '<p class="my-0 fw-700">14:30</p>' +
+    '<p class="my-0"><b>Team 586</b></p>',
+
+    '<p class="my-0 fw-700">14:30</p>' +
+    '<p class="my-0"><b>Team 990</b></p>'
 ];
 
 var p1Schedule = [
-    '<p class="my-0 fw-700">000</p>' +
-    '<p class="my-0"><b>Team NW-A1</b></p>' +
-    '<p class="my-0 fs-12"><b>Magic Chain</b></p>',
+    '<p class="my-0 fw-700">15:00</p>' +
+    '<p class="my-0"><b>Team 009</b></p>',
 
 
-    '<p class="my-0 fw-700">000</p>' +
-    '<p class="my-0"><b>Team NW-A1</b></p>' +
-    '<p class="my-0 fs-12"><b>Magic Chain</b></p>',
+    '<p class="my-0 fw-700">15:00</p>' +
+    '<p class="my-0"><b>Team 258</b></p>',
 
 
-    '<p class="my-0 fw-700">000</p>' +
-    '<p class="my-0"><b>Team NW-A1</b></p>' +
-    '<p class="my-0 fs-12"><b>Magic Chain</b></p>',
+    '<p class="my-0 fw-700">15:00</p>' +
+    '<p class="my-0"><b>Team 755</b></p>',
 
-    '<p class="my-0 fw-700">000</p>' +
-    '<p class="my-0"><b>Team NW-A1</b></p>' +
-    '<p class="my-0 fs-12"><b>Magic Chain</b></p>',
+    '<p class="my-0 fw-700">15:00</p>' +
+    '<p class="my-0"><b>Team 328</b></p>',
+
+    '<p class="my-0 fw-700">15:00</p>' +
+    '<p class="my-0"><b>Team 858</b></p>',
 
 
-    '<p class="my-0 fw-700">000</p>' +
-    '<p class="my-0"><b>Team NW-A1</b></p>' +
-    '<p class="my-0 fs-12"><b>Magic Chain</b></p>'
+    '<p class="my-0 fw-700">15:00</p>' +
+    '<p class="my-0"><b>Team 201</b></p>'
 ];
 
 var p2Schedule = [
-    '<p class="my-0 fw-700">000</p>' +
-    '<p class="my-0"><b>Team NW-A1</b></p>' +
-    '<p class="my-0 fs-12"><b>Magic Chain</b></p>',
+    '<p class="my-0 fw-700">15:30</p>' +
+    '<p class="my-0"><b>Team 634</b></p>',
 
 
-    '<p class="my-0 fw-700">000</p>' +
-    '<p class="my-0"><b>Team NW-A1</b></p>' +
-    '<p class="my-0 fs-12"><b>Magic Chain</b></p>',
+    '<p class="my-0 fw-700">15:30</p>' +
+    '<p class="my-0"><b>Team 118</b></p>',
 
 
-    '<p class="my-0 fw-700">000</p>' +
-    '<p class="my-0"><b>Team NW-A1</b></p>' +
-    '<p class="my-0 fs-12"><b>Magic Chain</b></p>',
+    '<p class="my-0 fw-700">15:30</p>' +
+    '<p class="my-0"><b>Team 487</b></p>',
 
-    '<p class="my-0 fw-700">000</p>' +
-    '<p class="my-0"><b>Team NW-A1</b></p>' +
-    '<p class="my-0 fs-12"><b>Magic Chain</b></p>',
+    '<p class="my-0 fw-700">15:30</p>' +
+    '<p class="my-0"><b>Team 267</b></p>',
 
 
-    '<p class="my-0 fw-700">000</p>' +
-    '<p class="my-0"><b>Team NW-A1</b></p>' +
-    '<p class="my-0 fs-12"><b>Magic Chain</b></p>'
+    '<p class="my-0 fw-700">15:30</p>' +
+    '<p class="my-0"><b>Team 773</b></p>',
+
+
+    '<p class="my-0 fw-700">15:30</p>' +
+    '<p class="my-0"><b>Team 737</b></p>'
 ];
 
 var p3Schedule = [
-    '<p class="my-0 fw-700">000</p>' +
-    '<p class="my-0"><b>Team NW-A1</b></p>' +
-    '<p class="my-0 fs-12"><b>Magic Chain</b></p>',
+    '<p class="my-0 fw-700">16:00</p>' +
+    '<p class="my-0"><b>Team 220</b></p>',
 
 
-    '<p class="my-0 fw-700">000</p>' +
-    '<p class="my-0"><b>Team NW-A1</b></p>' +
-    '<p class="my-0 fs-12"><b>Magic Chain</b></p>',
+    '<p class="my-0 fw-700">16:00</p>' +
+    '<p class="my-0"><b>Team 236</b></p>',
 
 
-    '<p class="my-0 fw-700">000</p>' +
-    '<p class="my-0"><b>Team NW-A1</b></p>' +
-    '<p class="my-0 fs-12"><b>Magic Chain</b></p>',
+    '<p class="my-0 fw-700">16:00</p>' +
+    '<p class="my-0"><b>Team 506</b></p>',
 
-    '<p class="my-0 fw-700">000</p>' +
-    '<p class="my-0"><b>Team NW-A1</b></p>' +
-    '<p class="my-0 fs-12"><b>Magic Chain</b></p>',
+    '<p class="my-0 fw-700">16:00</p>' +
+    '<p class="my-0"><b>Team 105</b></p>',
+
+    '<p class="my-0 fw-700">16:00</p>' +
+    '<p class="my-0"><b>Team 167</b></p>',
 
 
-    '<p class="my-0 fw-700">000</p>' +
-    '<p class="my-0"><b>Team NW-A1</b></p>' +
-    '<p class="my-0 fs-12"><b>Magic Chain</b></p>'
+    '<p class="my-0 fw-700">16:00</p>' +
+    '<p class="my-0"><b>Team / </b></p>'
 ];
 
 // total 24
 var hkbcolFinalists = [
-    'magic-chain',
-    'magic-chain',
-    'magic-chain',
-    'magic-chain',
-    'magic-chain'
+    // 'magic-chain',
+    // 'magic-chain',
+    // 'magic-chain',
+    // 'magic-chain',
+    // 'magic-chain'
 ]
 
 
@@ -149,5 +142,5 @@ for (let i = 0; i < p0Schedule.length; i++) {
 }
 
 function showWeb(index) {
-    window.open('https://hk.ibcol.org/2020-HKBCOL-Finalists/'+hkbcolFinalists[index]+'.html');
+    window.open('https://hk.ibcol.org/2020-HKBCOL-Finalists/' + hkbcolFinalists[index] + '.html');
 }
